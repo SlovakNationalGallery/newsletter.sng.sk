a002dc480e3e35a17f760bd91e04482a6e044149
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-04-22 14:32:38 +0200

    add tailwind + vue.js

---
+++ resources/js/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1 class="text-green-500">Chcete byť v obraze?</h1>
+</template>
+
+<script>
+
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="min-h-screen">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        <title>{{ config('app.name') }}</title>
+        @if (App::environment('production'))
+            {{-- @TODO: Clarity & GA --}}
+        @endif
+    </head>
+    <body class="min-h-screen">
+        <div id="app" class="bg-black min-h-screen flex flex-col"></div>
+        <script src="{{ mix('/js/app.js') }}"></script>
+    </body>
+</html>
