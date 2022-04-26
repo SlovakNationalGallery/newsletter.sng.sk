dc17c51ebf68e63dde7aa8af74481027adfc29c0
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-04-26 12:38:34 +0200

    fix clicking on option label NSNG-44

--- resources/js/components/NewsletterOption.vue
+++ resources/js/components/NewsletterOption.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex items-center mb-8">
       <div class="grow">
-        <label for="{{ props.option.id }}">
+        <label :for="props.option.id">
           <div class="text-5xl text-gray-50">{{ props.option.title }}</div>
           <div class="text-3xl text-white">{{ props.option.description }}</div>
           <div class="text-2xl text-gray-200 italic">{{ props.option.frequency }}</div>
         </label>
       </div>
       <div class="">
-        <input id="{{ props.option.id }}" type="checkbox" class="appearance-none w-14 h-14 bg-white border-8 border-gray-100 checked:bg-green checked:border-green" />
+        <input :id="props.option.id" type="checkbox" class="appearance-none w-14 h-14 bg-white border-8 border-gray-100 checked:bg-green checked:border-green" />
       </div>
     </div>
 </template>
