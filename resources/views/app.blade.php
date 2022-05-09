6b8296ed2d973d80081c9ba0771b71827f7b53f8
Igor Rjabinin <igor.rjabinin@gmail.com>
2022-05-09 21:14:49 +0200

    GA + Clarity code NSNG-46

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,18 @@
         {{-- /favicon --}}
         <title>{{ config('app.name') }}</title>
         @if (App::environment('production'))
-            {{-- @TODO: Clarity & GA --}}
+            {{-- GA4 --}}
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-WKNTKCP9B5"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WKNTKCP9B5');
+            </script>
+
+            {{-- Clarity --}}
+            <script type="text/javascript"> (function(c,l,a,r,i,t,y){ c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)}; t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i; y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y); })(window, document, "clarity", "script", "bs31c6aqvu"); </script>
         @endif
     </head>
     <body class="min-h-screen overscroll-none">
